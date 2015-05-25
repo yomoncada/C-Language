569c79b44ec24491f841826d70069f71feb905fd
Yonathan Moncada <ye_heme@hotmail.com>
2015-05-25 12:15:30 -0430

    Create multiplication_tables.c

---
+++ multiplication_tables.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main(){
+    int i=1, j;
+    printf("\t\t\tTablas de la Multiplicación:\n");
+    for(i=1;i<=10;i++){
+        printf("\nTabla del %d:\n",i);
+        for(j=1;j<=10;j++){
+        printf("%d * %d = %d\t\n",i,j,i*j);
+        }
+    }
+    return 0;
+}
