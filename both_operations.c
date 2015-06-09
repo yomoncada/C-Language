8faf42e12c6d0bf6cb630542c1d1aae838f46f5c
Yonathan Moncada <ye_heme@hotmail.com>
2015-06-08 19:40:12 -0430

    Create both_operations.c

---
+++ both_operations.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+int main(){
+    int i, num, cant, producto=1, suma=0;
+    pregunta:
+    printf("Introduzca la cantidad de valores que desea ingresar: ");
+    scanf("%d",&cant);
+    if(cant<10){
+        printf("\nLa cantidad debe ser mayor a diez para efectuar correctamente la operación.\n");
+        printf("\n");
+        goto pregunta;
+    }
+    else{
+        int valor[cant];
+        printf("\n");
+        for(i=0;i<=cant-1;i++){
+            printf("Introduzca el valor número %d: ",i+1);
+            scanf("%d",&num);
+            valor[i] = num;
+            if(i<=5-1){
+                suma = suma + valor[i];
+            }
+        }
+        for(i=cant-1;i>=cant-5;i--){
+            producto = producto * valor[i];
+        }
+        printf("\nLa suma de los primeros cinco valores es: %d",suma);
+        printf("\nEl producto de los ultimos cinco valores es: %d",producto);
+        return 0;
+    }
+}
