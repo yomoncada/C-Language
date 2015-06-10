5d595a936b88e4b4532110c073e8984dc4a22014
Yonathan Moncada <ye_heme@hotmail.com>
2015-06-10 11:51:21 -0430

    Update reverse_order.c

--- reverse_order.c
+++ reverse_order.c
@@ -1,18 +1,26 @@
 #include<stdio.h>
 int main(){
     int i, cant, num;
+    pregunta:
     printf("¿Cuántos valores desea que el programa lea?: ");
     scanf("%d",&cant);
     printf("\n");
-    int valores[cant];
-    for(i=0;i<=cant-1;i++){
-        printf("Introduzca el valor número %d: ",i+1);
-        scanf("%d",&num);
-        valores[i] = num;
+    if(cant==0){
+        printf("¿Usted no quiere introducir ningún elemento?.\n");
+        printf("\n");
+        goto pregunta;
     }
-    printf("\n");
-    for(i=cant-1;i>=0;i--){
-        printf("%d\t",valores[i]);
+    else{
+        int valor[cant];
+        for(i=0;i<=cant-1;i++){
+            printf("Introduzca el valor número %d: ",i+1);
+            scanf("%d",&num);
+            valor[i] = num;
+        }
+        printf("\n");
+        for(i=cant-1;i>=0;i--){
+            printf("%d\t",valor[i]);
+        }
     }
     return 0;
 }
